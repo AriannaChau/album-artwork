294e7546dd21d7289c3934ebedb6c54665f9dc2c
arianna chau and tyler allen <arianna+tyler@>
2017-06-15 12:04:02 -0700

    make inpuput form and output functional

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+//---------------BACKEND---------------//
+//Player Constructor
+function Boa(artist, album, year, artwork) {
+  this.artist = artist;
+  this.album = album;
+  this.year = year;
+  this.artwork = artwork;
+}
+
+var clicks = 0;
+function onClick() {
+    clicks += 1;
+    // document.getElementById("add-address").innerHTML = clicks;
+};
+//---------------FRONTEND---------------//
+$(function(){
+  $(".input-form").submit(function(event){
+    event.preventDefault();
+    var inputArtist = $("#artist").val();
+    var inputAlbum = $("#album").val();
+    var inputYear = $("#year").val();
+    var inputArtwork = $("#artwork").val();
+    var newBoa = new Boa(inputArtist, inputAlbum, inputYear, inputArtwork);
+
+    console.log(newBoa);
+
+    $("#gallery").append('<div id="album' + clicks + '"><h2>' + newBoa.artist + '</h2><h2>' + newBoa.album + '</h2><h3>' + newBoa.year + '</h3></div>');
+
+    $("#album" + clicks).css("background-image",'url("' + newBoa.artwork + '")');
+  });
+});
+
+// <img src="'+ newBoa.artwork + '"/>
